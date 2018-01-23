9c2edde80ee35dcdbe7360421b5e7b51c7fe28c9
2104dalibioulleanzul <2104dalibioulleanzul@PC9774.domepfc.net>
2018-01-23 21:33:37 +0100

    yooooo

---
+++ nez/src/nez/DemoAliment.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nez;
+
+import com.oracle.jrockit.jfr.Producer;
+import javax.swing.GroupLayout;
+
+/**
+ *
+ * @author 2104dalibioulleanzul
+ */
+public class DemoAliment {
+    public static void main(String[] args) {
+        Aliment[] produitList = new Aliment[10];
+        //produitList[0] = new Aliment("cornichon","legumes" ,60,1);
+        Category cat= new Category("Viande");
+        Category[] cat2 = new Category[3];
+        
+        
+        //String catV = ne 
+        
+        produitList[0] = new Aliment("cornichon",cat ,60,1);
+        
+        for (Aliment aliment : produitList) 
+        {
+           
+            System.out.println(Aliment);   
+        }
+        
+        
+        
+        
+    }
+    
+}
